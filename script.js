4c4808ab9d7e100de63001632de4594a1bc52709
Munish <hmunish5@gmail.com>
2023-06-21 14:47:39 +0200

    Add leaflet library

--- script.js
+++ script.js
@@ -10,3 +10,28 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+
+if(navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(function(pos){
+        const {latitude:lat, longitude:lon} =  pos.coords;
+
+        
+var map = L.map('map').setView([lat, lon], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+map.on('click', function(mapEvent){
+    const {lat, lng} = mapEvent.latlng;
+    
+    L.marker([lat, lng]).addTo(map)
+        .bindPopup('Workout')
+        .openPopup();
+        }, function(){
+            alert('Cannot access your location');
+        })
+})
+
+}
